7f867801b9e8d0c7e3538efcd4930fe58e5b2a60
ForeverZyh <zyhzyhzyh@pku.edu.cn>
2017-09-21 18:34:24 +0800

    correct ex3

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display backtrace", mon_backtrace },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
